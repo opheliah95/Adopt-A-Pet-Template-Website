e0ba425f38d18d25769eeadddf4d19f6709ddd74
opheliah95 <opheliah95@gmail.com>
2022-03-29 20:26:48 +0100

    [feature] Added image card component

---
+++ web-app/src/ImageCards.js
@@ -0,0 +1,20 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
+
+function SingleCard() {
+
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Sample Animal</Card.Title>
+        <Card.Text>
+          This is the sample card element
+        </Card.Text>
+        <Button variant="primary">More Info</Button>
+      </Card.Body>
+    </Card>
+  )
+
+}
+export default SingleCard;
