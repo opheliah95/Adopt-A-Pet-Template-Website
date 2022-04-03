477bb1c679f1e055706aa9514ae499417575a15e
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2022-04-04 00:33:20 +0100

    [Refactor] Added state variable

--- web-app/src/ImageCards.js
+++ web-app/src/ImageCards.js
@@ -1,23 +1,28 @@
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import animals from './Animals';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import animals from "./Animals";
+import React from "react";
 
-function SingleCard(name="Test", description="Test") {
-
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px160" />
-      <Card.Body>
-        <Card.Title>{this.name}</Card.Title>
-        <Card.Text>
-        {this.description}
-        </Card.Text>
-        <Button variant="primary">More Info</Button>
-      </Card.Body>
-    </Card>
-  )
-
+class SingleCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Test Name",
+      description: "Test description",
+    };
+  }
+  render() {
+    return (
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>{this.state.name}</Card.Title>
+          <Card.Text>{this.state.description}</Card.Text>
+          <Button variant="primary">More Info</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
-
 
 export default SingleCard;
