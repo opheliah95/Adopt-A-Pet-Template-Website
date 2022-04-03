7d6d5916e960755be7b2fc3a3100db5689f90eb2
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2022-04-03 22:43:40 +0100

    [ImageCard] Start to work on variables

---
+++ web-app/src/Animals.js
@@ -0,0 +1,15 @@
+export const animals = [
+    {
+      id: 1,
+      name: "Ginger",
+      species: "Cat",
+      description: "Ginger is a 3 year old cat and she is super friendly"
+    },
+
+    {
+        id: 2,
+        name: "Mike",
+        species: "Dog",
+        description: "Mike is a 2 year old chihuahua"
+      }
+];
--- web-app/src/ImageCards.js
+++ web-app/src/ImageCards.js
@@ -1,15 +1,16 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import animals from './Animals';
 
-function SingleCard() {
+function SingleCard(name="Test", description="Test") {
 
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px160" />
       <Card.Body>
-        <Card.Title>Sample Animal</Card.Title>
+        <Card.Title>{this.name}</Card.Title>
         <Card.Text>
-          This is the sample card element
+        {this.description}
         </Card.Text>
         <Button variant="primary">More Info</Button>
       </Card.Body>
@@ -17,4 +18,6 @@ function SingleCard() {
   )
 
 }
+
+
 export default SingleCard;
