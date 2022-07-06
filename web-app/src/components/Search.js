9fa5dfa8ca0e7b4139714afde04c67150e2e0e0b
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2022-07-06 17:47:31 +0100

    feat: initial search bar

--- web-app/src/components/Search.js
+++ web-app/src/components/Search.js
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 
 const SearchBar = (prop) => {
     // something to search
-    const { searchInput, setSearchInput } = useState("");
+    const [searchInput, setSearchInput] = useState("");
+
+    // Function to update value of search
+    const updateVal = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+        console.log(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const name = e.target[0].value;
+        setSearchInput(name);
+        console.log(`you entered ${name}`);
+    }
 
     return (
         <Container className="searchBar">
-            <Form className="searchInput">
+            <Form className="searchInput" onSubmit={handleSubmit}>
                 <Form.Label>Search A Pet</Form.Label>
-                <Form.Control placeholder="Enter a pet" />
+                <Form.Control placeholder="Enter a pet" value={searchInput}
+                    onChange={updateVal} />
+                <Button type='submit'> Search</Button>
             </Form>
-            <Button> Search</Button>
+
         </Container>
     );
 }
