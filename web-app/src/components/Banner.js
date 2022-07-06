1d25f1d587813d02099736e66e8aba811733fcb0
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2022-07-06 16:50:19 +0100

    feat: new banner

---
+++ web-app/src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../css/Banner.css';
+
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <h1 className="banner-header">Welcome to animal rescue</h1>;
+        </div>
+    );
+    
+
+}
+
+export default Banner;
--- web-app/src/index.js
+++ web-app/src/index.js
@@ -4,9 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import SingleCard from "./ImageCards";
 import {Row, Col} from "react-bootstrap";
 import Container from 'react-bootstrap/Container';
-
+import Banner from './components/Banner'
 ReactDOM.render(
   <React.StrictMode>
+    <Banner />
     <Container>
       <Row xs={1} md={3} xl={6}>
       <SingleCard />
