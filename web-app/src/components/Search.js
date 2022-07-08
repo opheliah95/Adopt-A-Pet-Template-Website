2d19ed1bb2e1987daa146dca2a3f74bdc4edac20
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2022-07-08 23:27:26 +0100

    feat: restyled search bar

--- web-app/src/components/Search.js
+++ web-app/src/components/Search.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
+import {Row, Col} from 'react-bootstrap'
 
 const SearchBar = (prop) => {
     // something to search
@@ -24,10 +25,14 @@ const SearchBar = (prop) => {
     return (
         <Container className="searchBar">
             <Form className="searchInput" onSubmit={handleSubmit}>
-                <Form.Label>Search A Pet</Form.Label>
-                <Form.Control placeholder="Enter a pet" value={searchInput}
-                    onChange={updateVal} />
-                <Button type='submit'> Search</Button>
+                <Row>
+                    <Col md={2} lg={2} xs={1}><Form.Label>Search A Pet</Form.Label></Col>
+                    <Col xs={3} lg={6} md={6}>
+                        <Form.Control placeholder="Enter a pet" value={searchInput}
+                            onChange={updateVal} />
+                    </Col>
+                    <Col md={3} lg={3} xs={1}><Button type='submit'> Search</Button></Col>
+                </Row>
             </Form>
 
         </Container>
