6540ad13664687d778cb1e368535b2258c491ecd
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2022-07-11 10:31:27 +0100

    fix: search bar width

--- web-app/src/components/Search.js
+++ web-app/src/components/Search.js
@@ -2,11 +2,39 @@ import React, { useState, useEffect } from "react";
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
-import {Row, Col} from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
+import '../css/SearchBar.css'
 
 const SearchBar = (prop) => {
+    // inline styling
+    const searchStyle = {
+        container: isWide => (
+            {
+                justifyContent: 'right',
+                alignItems: 'right',
+                color: 'blue',
+                display: 'flex'
+            }
+        )
+
+    }
+
     // something to search
     const [searchInput, setSearchInput] = useState("");
+
+    // listen to window size change
+    const mediaMatch = window.matchMedia('(min-width: 1500px)');
+    const [matches, setMatches] = useState(mediaMatch.matches);
+
+    // size of search box
+    const searchBoxSize = matches? 7: 6;
+
+    // when input is updated, update the match
+    useEffect(() => {
+        const handler = e => setMatches(e.matches);
+        mediaMatch.addListener(handler);
+        return () => mediaMatch.removeListener(handler);
+    })
 
     // Function to update value of search
     const updateVal = (e) => {
@@ -25,13 +53,13 @@ const SearchBar = (prop) => {
     return (
         <Container className="searchBar">
             <Form className="searchInput" onSubmit={handleSubmit}>
-                <Row>
+                <Row className="grid-center">
                     <Col md={2} lg={2} xs={1}><Form.Label>Search A Pet</Form.Label></Col>
-                    <Col xs={3} lg={6} md={6}>
+                    <Col xs={3} lg={searchBoxSize} md={6}>
                         <Form.Control placeholder="Enter a pet" value={searchInput}
                             onChange={updateVal} />
                     </Col>
-                    <Col md={3} lg={3} xs={1}><Button type='submit'> Search</Button></Col>
+                    <Col md={3} lg={3} xs={1} style={searchStyle.container(matches)}><Button type='submit'> Search</Button></Col>
                 </Row>
             </Form>
 
